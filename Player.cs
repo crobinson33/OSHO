66b81a9650ebcd3e0a152427e2cec48ec3c36af0
Cody Robinson <crobinson33@gmail.com>
2014-04-10 17:38:31 -0700

    Initial commit
    bitches

---
+++ Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceBagel;
+
+namespace OSHO
+{
+    public class Player : BaseObject
+    {
+        public Texture atexture;
+        public AnimatedSprite asprite;
+        public Animation defaultAnimation;
+        public Animation downRunAnimation;
+
+        public Player(string tag, Vector2 position) : base(tag)
+        {
+            Console.WriteLine("player start...");
+            this.position = position;
+
+
+            atexture = new Texture("HunchWideSprites.png");
+
+            asprite = new AnimatedSprite(atexture, 64, 64);
+
+            
+            // Create animations
+            downRunAnimation = new Animation("default", 10, 10);
+            defaultAnimation = new Animation("downRun", 11, 7);
+
+            // Add animations
+            asprite.AddAnimation(defaultAnimation);
+            asprite.AddAnimation(downRunAnimation);
+
+            // Test animation
+            asprite.animationController.SetActiveAnimation(downRunAnimation);
+
+            Console.WriteLine("player end...");
+        }
+
+        public override void Update(float deltaTime)
+        {
+            //Console.WriteLine("getting called...");
+            base.Update(deltaTime);
+        }
+
+        public override void Draw(Surface surface)
+        {
+            //Console.WriteLine("getting called...");
+            surface.Draw(asprite, new Vector2(300, 300));
+
+
+            base.Draw(surface);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceBagel;
+
+namespace OSHO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            Level level1 = game.AddLevel();
+            game.SetCurrentLevel(0);
+
+            Player player1 = new Player("one", new Vector2(0, 0));
+
+            level1.AddObject(player1);
+
+            game.Start();
+        }
+    }
+}
